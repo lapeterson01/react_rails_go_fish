28b89eb52446b947233500628747390573d0e27c
Logan Peterson <loganpeterson@Logans-MacBook-Pro.local>
2018-12-05 17:35:48 -0500

    added sign up and sign in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,29 @@
 class SessionsController < ApplicationController
-  def index; end
+  def index
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by_username(user_params['username'])
+    return redirect_to root_path, notice: signin_notice('no user') unless user
+
+    unless user.authenticate(user_params['password'])
+      return redirect_to root_path, notice: signin_notice('wrong password')
+    end
+
+    session[:current_user], session[:host] = user.id, []
+    redirect_to games_path, notice: 'Logged in successfully'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def signin_notice(context)
+    return "Username #{user_params['username']} not found in the system" if context == 'no user'
+
+    return 'Username/password combination incorrect' if context == 'wrong password'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :sessions, only: %i[index]
+  resources :sessions, only: %i[index create]
+  resources :users,    only: %i[new create]
+  resources :games,    only: %i[index]
   root 'sessions#index'
 end
