317f9789ba4c36b7507766369c191170e71cffd6
Logan Peterson <lapeterson01@gmail.com>
2019-12-04 17:59:49 +0300

    Minor refactor

--- db/migrate/20181206155051_add_winner_to_games.rb
+++ db/migrate/20181206155051_add_winner_to_games.rb
@@ -1,5 +1,5 @@
 class AddWinnerToGames < ActiveRecord::Migration[5.2]
   def change
-    add_reference :games, :winner, foreign_key: { to_table: :users }r
+    add_reference :games, :winner, foreign_key: { to_table: :users }
   end
 end
